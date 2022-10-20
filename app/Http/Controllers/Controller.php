4c9a11c6d777c045d1ddfefa63ebf07df78df763
“GabrielSilva-PMK” <gabriel.silva@pmk.com.br>
2022-10-20 11:55:55 -0300

    Add @OA in Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,13 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(title="My First API", version="1.0.0")
+ * @OA\Get(
+ *      path="/",
+ *      @OA\Response(response="200", description="Home page")
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
